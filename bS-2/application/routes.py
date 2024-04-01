dfa066d9e27270fe8901d2b3a50583455740016f
ms-starryvoid <donamattel@gmail.com>
2024-04-01 23:26:57 +0530

    ss

--- bS-2/application/routes.py
+++ bS-2/application/routes.py
@@ -32,13 +32,13 @@ def log():
 def reg():
     #code to recieve data from the server insert to mongo db with user id
     data=request.json
-    print(data)
+    #print(data)
     #data = msgpack.loads(data)
     length = data['data']['len']
     description = data['data']['descrip']
     user_id = data['data']['user_id']
     try:
-        insert_result = db.collection10.insert_one({"length": length, "description": description, "user_id": user_id})
+        insert_result = db.collection1.insert_one({"length": length, "description": description, "user_id": user_id})
         return {"success": True, "message": f"Data inserted with ID: {insert_result.inserted_id}"}, 201  # Created status code
     except pymongo.errors.PyMongoError as e:
         return {"success": False, "message": f"Error inserting data: {str(e)}"}, 500  # Internal Server Error
