70a1b22f031b7b2593c5344106d50d92ec0bb3bc
ms-starryvoid <donamattel@gmail.com>
2024-04-01 22:45:16 +0530

    ll

--- bS-2/application/routes.py
+++ bS-2/application/routes.py
@@ -38,7 +38,7 @@ def reg():
     description = data['data']['descrip']
     user_id = data['data']['user_id']
     try:
-        insert_result = db.fing1.insert_one({"length": length, "description": description, "user_id": user_id})
+        insert_result = db.collection10.insert_one({"length": length, "description": description, "user_id": user_id})
         return {"success": True, "message": f"Data inserted with ID: {insert_result.inserted_id}"}, 201  # Created status code
     except pymongo.errors.PyMongoError as e:
         return {"success": False, "message": f"Error inserting data: {str(e)}"}, 500  # Internal Server Error
